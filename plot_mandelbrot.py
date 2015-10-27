1ac7766de4e12f86815ff71aadda7349a8d53193
acsams2 <sams6@clemson.edu>
2015-10-27 12:03:48 -0400

    Allison/Adam
    added function to plot and save

--- plot_mandelbrot.py
+++ plot_mandelbrot.py
@@ -2,6 +2,12 @@ import matplotlib.pyplot as plt
 
 # plot figure and show in window
 def plot_mandelbrot(mandelbrot,xmin,xmax,ymin,ymax):
+    plt.imshow(mandelbrot);
+    plt.axis((xmin,xmax,ymin,ymax));
 
 # plot figure and save to file
 def save_mandelbrot(mandelbrot,xmin,xmax,ymin,ymax,iteration):
+    plt.imshow(mandelbrot);
+    plt.axis((xmin,xmax,ymin,ymax));
+    filename = 'Mandelbrot' + iteration;
+    savefig(filename+'.eps');
